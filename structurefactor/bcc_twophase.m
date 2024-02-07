524929feeec5b2b1fe35821ed3ce9da0ac9649e7
Byeongdulee <blee@anl.gov>
2024-02-07 15:45:14 -0600

    new functions added.

--- structurefactor/bcc_twophase.m
+++ structurefactor/bcc_twophase.m
@@ -3,12 +3,17 @@ function [y, Fq, Iq] = bcc_twophase(varargin)
 q = varargin{1};
 a = varargin{2}(1);
 d = a;
-volf = varargin{2}(2);
+volf = -1;
+if numel(varargin{2})==2
+    volf = varargin{2}(2);
+end
+
 d_rho = varargin{3};
 domainsize = 5000;
 microstrain = 0.02;
 DW = 0.1;
 ispowder = 1;
+dR = -1;
 
 if numel(varargin) > 3
     domainsize = varargin{4};
@@ -19,6 +24,16 @@ end
 if numel(varargin) > 3
     DW = varargin{6};
 end
+
+vol = a^3;
+R = (volf*vol/2/(4*pi/3))^(1/3);
+
+if numel(varargin) > 6
+    R = varargin{7};
+    dR = varargin{8};
+    volf = 3*pi/3*R^3/a^3;
+end
+
 wavelength = 1; % for peakwidth calculation only.
 % thick = zeros(size(relcomp));
 % maxh = 5;
@@ -32,8 +47,6 @@ qhk = [];
 
 %a1 = [Dsp, 0];
 %a2 = [-1/2*Dsp, -sqrt(3)/2*Dsp];
-vol = a^3;
-R = (volf*vol/2/(4*pi/3))^(1/3);
 if volf>=0.6802
     fprintf('For BCC, volf of sphere cannot excced 0.6802.\n');
     return
@@ -44,7 +57,6 @@ j = sqrt(-1);
 
 m = 1;
 coef = d_rho;
-
 
 [h,k,l]=ndgrid(-maxh:maxh, -maxk:maxk, -maxl:maxl);
 h = h(:);
@@ -65,6 +77,13 @@ qhk = 2*pi./dv;
 Fq = coef*sphereamp(qhk,R).*(1+...
     exp(-j*2*pi*sum(bsxfun(@times, hkl, [1/2, 1/2, 1/2]),2)));
 
+if dR ~= -1
+    Pq2 = SchultzsphereFun(qhk, R, dR*R);
+    Fq = Fq./sqrt(Pq2);
+    Pq = SchultzsphereFun(q, R, dR*R);
+else
+    Pq = ones(size(q));
+end
 
 % for h=-maxh:maxh
 %     for k=-maxk:maxk
@@ -103,3 +122,4 @@ for h = 1:numel(qhk)
     t = pseudovoigt(q, [Iq(h), qh, 0.00025, w]);
     y = y + t(:);
 end
+y = y.*Pq;
--- structurefactor/hex_tetrahedron.m
+++ structurefactor/hex_tetrahedron.m
@@ -1,9 +1,25 @@
 function Sq = hex_tetrahedron(varargin)
 % y = hex_twophase(q, [lattice_const, L, th0], delta_rho, domainsize, microstrain, DW)
+% This also calculates the 2D form factor of tetrahedron.
 q = varargin{1};
 a = varargin{2}(1);
 L = varargin{2}(2);
 th0 = varargin{2}(3)*pi/180;
+ispq = false;
+for indx=1:2:numel(varargin)
+    if ischar(varargin{indx})
+        switch varargin{indx}
+            case 'pq'
+                ispq = varargin{indx+1};
+        end
+    end
+end
+if ispq
+    Qvector = [q(:), zeros(size(q(:))), zeros(size(q(:)))];
+    Sq = tetrahedron_2d_pq(Qvector, L);
+    return
+end
+
 domainsize = 5000;
 microstrain = 0.02;
 DW = 0.1;
@@ -21,28 +37,31 @@ if numel(varargin) > 5
     DW = varargin{6};
 end
 
+
+
 wavelength = 1; % for peakwidth calculation only.
 % thick = zeros(size(relcomp));
 % maxh = 5;
 
 DW = DW*a;
 
-as = [2*pi,    2*pi/sqrt(3),   0];
-bs = [0,    4*pi/sqrt(3),   0];
-cs = [0,    0,   1];
+as = [2*pi,    2*pi/sqrt(3),   0]/a;
+bs = [0,    4*pi/sqrt(3),   0]/a;
+cs = [0,    0,   1]/a;
 
 hklmat = load('hklhex.mat');
 hkl = hklmat.hkl;
 hklhex = hklmat.hklhex;
 m = hklmat.m;
 
-Q0 = hkl'*[as;bs;cs]/(a);
-%Q0 = Q0';
+Q0 = hkl'*[as;bs;cs];
 qx0 = Q0(:,1);
 qy0 = Q0(:,2);
 %qz0 = Q0(:,3);
 qr = sqrt(qx0.^2+qy0.^2);
-Fq2 = hex_tetrahedron_Fq_squared(L/a, th0);
+
+%Q0 = Q0';
+Fq2 = hex_tetrahedron_Fq_squared(m, hklhex, hkl, as, bs, cs, L, th0);
 
 y = zeros(size(q));
 y = y(:);
@@ -60,8 +79,6 @@ Lorentzfactor = qr.^(dim_cell-1);
 % final 
 %Iqa = factor*Iq./Pq./Lorentzfactor;
 
-
-
 for h = 1:numel(qr)
     qh = qr(h);
     w = peakwidth(qh, wavelength, domainsize, microstrain);
@@ -72,55 +89,48 @@ Gq = exp(-(DW)^2*q.^2);
 Sq = 1+(Zq - 1).*Gq;
 
 
-function Fq_squared = hex_tetrahedron_Fq_squared(La_ratio, th0)
-%load hklhex.mat;
-m = m';
-% hklhex and hkl
-
-% % for the lattice constant 1:
-% as = [6.2832,    3.6276,   0];
-% bs = [0,    7.2552,   0];
-% cs = [0,    0,   1];
-Q = hklhex'*[as;bs;cs];
-%qx = Q(:,1);
-%qy = Q(:,2);
-%qz = Q(:,3);
-
-% Inversion rotation matrix R; 
-% when a object rotates by R in the real space,
-% the same effect can be computed by rotating inv(R) of Q coordinates..
-R = [cos(th0), sin(th0), 0;-sin(th0), cos(th0),0;0,0,1];
-qtmp = Q*R';
-F = saxstetrahedron(qtmp(:,1),qtmp(:,2),qtmp(:,3), La_ratio);
-n0 = 1;
-Fq = zeros(11, 1);
-mcumsum = cumsum(m);
-for i=1:numel(m)
-    n = n0:mcumsum(i);
-    Fq(i) = sum(F(n));
-    n0=mcumsum(i)+1;
+    function Fq_squared = hex_tetrahedron_Fq_squared(m, hklhex, hkl, as, bs, cs, edgelength, th0)
+        m = m';
+        Q = hklhex'*[as;bs;cs];
+        
+        % Inversion rotation matrix R; 
+        % when a object rotates by R in the real space,
+        % the same effect can be computed by rotating inv(R) of Q coordinates..
+        R = [cos(th0), sin(th0), 0;-sin(th0), cos(th0),0;0,0,1];
+        qtmp = Q*R';
+        F = saxstetrahedron(qtmp(:,1),qtmp(:,2),qtmp(:,3), edgelength);
+        n0 = 1;
+        Fq = zeros(11, 1);
+        mcumsum = cumsum(m);
+        for i=1:numel(m)
+            n = n0:mcumsum(i);
+            Fq(i) = sum(F(n));
+            n0=mcumsum(i)+1;
+        end
+        Iq = abs(Fq).^2./m;
+        % form factor
+        Qv = hkl'*[as;bs;cs];
+        Pq = tetrahedron_2d_pq(Qv, edgelength);
+        Fq_squared = Iq./Pq;
+    end
 end
-Iq = abs(Fq).^2./m;
-% form factor
-Qreduced = hkl'*[as;bs;cs];
-%Qreduced = Qreduced';
-qx_reduced = Qreduced(:,1);
-qy_reduced = Qreduced(:,2);
-qz_reduced = Qreduced(:,3);
 
-qr_reduced = sqrt(qx_reduced.^2+qy_reduced.^2);
-q0 = zeros(size(qr_reduced));
-Q = [qr_reduced, qz_reduced];
-Pq = q0;
-k=0;
-for ang0=-pi/3+pi/180:pi/180:pi/3
-    R = [cos(ang0), sin(ang0);-sin(ang0), cos(ang0)];
-    q_reduced = Q*R';
-    F = saxstetrahedron(q_reduced(:,1),q_reduced(:,2),q0, La_ratio);
-    Pq = Pq+abs(F).^2;
-    k = k + 1;
-end
-Pq = Pq/k;
-Fq_squared = Iq./Pq;
-end
+function Pq = tetrahedron_2d_pq(Qv, edgelength)
+    qx_reduced = Qv(:,1);
+    qy_reduced = Qv(:,2);
+    qz_reduced = Qv(:,3);
+    
+    qr_reduced = sqrt(qx_reduced.^2+qy_reduced.^2);
+    q0 = zeros(size(qr_reduced));
+    Q = [qr_reduced, qz_reduced];
+    Pq = q0;
+    k=0;
+    for ang0=-pi/3+pi/180:pi/180:pi/3
+        R = [cos(ang0), sin(ang0);-sin(ang0), cos(ang0)];
+        q_reduced = Q*R';
+        F = saxstetrahedron(q_reduced(:,1),q_reduced(:,2),q0, edgelength);
+        Pq = Pq+abs(F).^2;
+        k = k + 1;
+    end
+    Pq = Pq/k;
 end
