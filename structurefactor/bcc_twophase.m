b53d659a5af7b1333e72306c4c6ca9d10577d914
Byeongdulee <blee@anl.gov>
2024-02-09 23:25:59 -0600

    two phase bcc and hex changed.

--- structurefactor/bcc_twophase.m
+++ structurefactor/bcc_twophase.m
@@ -32,6 +32,7 @@ if numel(varargin) > 6
     R = varargin{7};
     dR = varargin{8};
     volf = 3*pi/3*R^3/a^3;
+    DW = -DW;
 end
 
 wavelength = 1; % for peakwidth calculation only.
@@ -57,26 +58,28 @@ j = sqrt(-1);
 
 m = 1;
 coef = d_rho;
+HKL = load('bcchkl.mat');
+hkl = HKL.HKL(:,1:3);
+dv = HKL.HKL(:,4)/100*a;
+% [h,k,l]=ndgrid(-maxh:maxh, -maxk:maxk, -maxl:maxl);
+% h = h(:);
+% k = k(:);
+% l = l(:);
+% dv = a./sqrt(h.^2+k.^2+l.^2);
+% hkl = [h,k,l];
+% t = h==0 & k==0 & l==0;
+% hkl(t,:) = [];
+% dv(t) = [];
 
-[h,k,l]=ndgrid(-maxh:maxh, -maxk:maxk, -maxl:maxl);
-h = h(:);
-k = k(:);
-l = l(:);
-dv = a./sqrt(h.^2+k.^2+l.^2);
-hkl = [h,k,l];
-t = h==0 & k==0 & l==0;
-hkl(t,:) = [];
-dv(t) = [];
-
-[~, index, ind2] = unique(dv, 'rows', 'stable');
-% multiplicity
-multip = histc(ind2, 1:max(ind2));
-hkl = hkl(index, :);
-dv = dv(index);
+%[~, index, ind2] = unique(dv, 'rows', 'stable');
+%% multiplicity
+%multip = histc(ind2, 1:max(ind2));
+%hkl = hkl(index, :);
+%dv = dv(index);
 qhk = 2*pi./dv;
 Fq = coef*sphereamp(qhk,R).*(1+...
     exp(-j*2*pi*sum(bsxfun(@times, hkl, [1/2, 1/2, 1/2]),2)));
-
+Fq = Fq/(4*pi/3*R^3);
 if dR ~= -1
     Pq2 = SchultzsphereFun(qhk, R, dR*R);
     Fq = Fq./sqrt(Pq2);
@@ -85,41 +88,31 @@ else
     Pq = ones(size(q));
 end
 
-% for h=-maxh:maxh
-%     for k=-maxk:maxk
-%         for l=-maxl:maxl
-%             dv = d./sqrt(h^2+k^2+l^2);
-%             qv = 2*pi/dv;
-%             if h==0 & k==0 & l==0
-%                 continue;
-%             end
-%             if qv > maxq
-%                 continue;
-%             end
-%             qhk(m) = qv;
-%             Fq(m) = coef*sphereamp(qv,R)*(1 + exp(-j*2*pi*dot([h, k, l], [1/2, 1/2, 1/2])));
-%             m = m + 1;
-%         end
-%     end
-% end
-% 
-% [qhk, ind, ic1] = unique(qhk);
-% multip = zeros(size(qhk));
-% for i=1:numel(qhk)
-%     multip(i) = sum(ic1==i);
-% end
-% Fq = Fq(ind);
 y = zeros(size(q));
 y = y(:);
 Iq = zeros(size(Fq));
+omega = 4*pi;
+cVol = a^3;
+factor = 1/omega*(2*pi)^3/cVol*1000;
 for h = 1:numel(qhk)
     qh = qhk(h);
-    Iq(h) = multip(h)*abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
+    if DW>0
+        Iq(h) = abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
+    else
+        Iq(h) = abs(Fq(h))^2;
+    end
+
+    %Iq(h) = multip(h)*abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
     if ispowder % then, Lorentz correction.
         Iq = Iq./qh.^2;
     end
     w = peakwidth(qh, wavelength, domainsize, microstrain);
-    t = pseudovoigt(q, [Iq(h), qh, 0.00025, w]);
-    y = y + t(:);
+    t = pseudovoigt(q, [1, qh, 0.00025, w]);
+    y = y + Iq(h)*t(:);
 end
-y = y.*Pq;
+y = y*factor;
+if DW<0
+    y(q>2*pi/min(dv)) = 1;
+    Sq = 1+(y-1).*exp(-DW^2*q.^2);
+end
+y = Sq.*Pq;
--- structurefactor/hex_twophase.m
+++ structurefactor/hex_twophase.m
@@ -31,7 +31,8 @@ R = sqrt(volf*area/pi);
 if numel(varargin) > 6
     R = varargin{7};
     dR = varargin{8};
-    volf = pi*R^2/area^3;
+    volf = pi*R^2/area;
+    DW = -DW;
 end
 
 wavelength = 1; % for peakwidth calculation only.
@@ -51,42 +52,22 @@ qhk = [];
 if volf>=0.9069
     fprintf('For hex, volf of cylinder cannot exceed 0.9069.\n');
     return
-else
-    fprintf('Radius of cylinder is %0.3f.\n', R);
 end
 j = sqrt(-1);
 
-m = 1;
-coef = d_rho*pi*R^2;
-for h=-maxh:maxh
-    for k=-maxk:maxk
-        dv = d./sqrt(4/3*(h.^2+h.*k+k.^2));
-        qv = 2*pi/dv;
-        if h==0 & k==0
-            continue;
-        end
-        if qv > maxq
-            continue;
-        end
-        qhk(m) = qv;
-        Fq(m) = 0;
-%        for i=1:length(thick)
-            Fq(m) = Fq(m) + coef*besseljc(qv*R);
-%        end
-        m = m + 1;
-    end
-end
+HKL = load('bcchkl.mat');
+hkl = HKL.HKL(:,1:3);
+dv = HKL.HKL(:,4)/100*a;
+qhk = 2*pi./dv;
 
-[qhk, ind, ic1] = unique(qhk);
-multip = zeros(size(qhk));
-for i=1:numel(qhk)
-    multip(i) = sum(ic1==i);
-end
+%Fq = coef*cylinder_amp(qhk,0,0,R,1,[0,0,1]).*(1+...
+%    exp(-j*2*pi*sum(bsxfun(@times, hkl, [1/2, 1/2, 1/2]),2)));
+%Fq = Fq/(4*pi/3*R^3);
 
-Fq = Fq(ind);
 if dR ~= -1
-    Pq2 = saxscylinder_CS(qhk(:), R, [1, 0], dR);
-    Fq = Fq./sqrt(Pq2);
+    %Pq2 = saxscylinder_CS(qhk(:), R, [1, 0], dR);
+    %Fq = Fq./sqrt(Pq2);
+    Fq = ones(size(qhk(:)));
     Pq = saxscylinder_CS(q(:), R, [1, 0], dR);
 else
     Pq = ones(size(q));
@@ -95,14 +76,28 @@ end
 y = zeros(size(q));
 y = y(:);
 Iq = zeros(size(Fq));
+omega = 2*pi;
+factor = 1/omega*(2*pi)^2/area/10;
+
 for h = 1:numel(qhk)
     qh = qhk(h);
-    Iq(h) = multip(h)*abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
+    if DW>0
+        Iq(h) = abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
+    else
+        Iq(h) = abs(Fq(h))^2;
+    end
+
+    %Iq(h) = multip(h)*abs(Fq(h))^2.*exp(-qh^2*DW^2); % DW factor
     if ispowder % then, Lorentz correction.
-        Iq = Iq./qh.^2;
+        Iq = Iq./qh;
     end
     w = peakwidth(qh, wavelength, domainsize, microstrain);
     t = pseudovoigt(q, [Iq(h), qh, 0.00025, w]);
-    y = y + t(:);
+    y = y + Iq(h)*t(:);
 end
-y = y.*Pq;
+y = y*factor;
+if DW<0
+    y(q>2*pi/min(dv)) = 1;
+    Sq = 1+(y-1).*exp(-DW^2*q.^2);
+end
+y = Sq.*Pq;
